a0b6c751560c41d041cedf6968e93f3b3b4fa798
harshithtunuguntla <harshithtunuguntla@gmail.com>
2022-01-22 15:17:28 +0530

    working of upload, not the correct format

--- certify/certifyapi/testing_images.py
+++ certify/certifyapi/testing_images.py
@@ -1,4 +1,96 @@
 from PIL import Image
-from PIL import ImageDraw
+from PIL import ImageDraw,ImageFont
+# import requests
 
-img = Image.open('car.png')
+
+font_path = "media/fonts/MonoLisa-Black.ttf"
+font = ImageFont.truetype(font_path, 60)
+
+img = Image.open('media/images/certificate.png')
+
+I1 = ImageDraw.Draw(img)
+I1.text((610, 740), "Harshith Sai Tunuguntla",font=font, fill=(0, 0, 0))
+img.show()
+# img.save('result.png')
+print(img)
+
+
+import pyrebase
+import config
+
+firebaseConfig = {
+    "apiKey": config.apiKey,
+    "authDomain": config.authDomain,
+    "projectId": config.projectId,
+    "databaseURL": config.databaseURL,
+    "storageBucket": config.storageBucket,
+    "messagingSenderId": config.messagingSenderId,
+    "appId": config.appId,
+    "measurementId": config.measurementId,
+    "serviceAccount": config.serviceAccount
+}
+
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+storage = firebase.storage()
+
+metadata = '"contentType": "image/jpeg" "size":"262144"'
+# metadata = '"contentType": "image/jpeg","size":"262144"'
+# metadata = {"size":262144}
+
+
+
+
+filepath = "cer/" + "cert1"
+
+
+
+# storage.child(filepath).put("media/images/certificate.png",metadata)
+
+# bucket = "imageupload-19537"
+
+# blob = bucket.blob(filepath, chunk_size=262144) # 256KB
+# blob.upload_from_file(img)
+
+# img_url = storage.child(filepath).get_url(None)
+
+# print(img_url)
+
+# --------------------------------
+
+# import io
+# rgb_im = img.convert('RGB')
+# buffer = io.BytesIO()
+# rgb_im.save(buffer, format='JPEG', quality=75)
+
+# # You probably want
+# desiredObject = buffer.getbuffer()
+
+# print(desiredObject)
+# filepath = "cer/" + "certf"
+# storage.child(filepath).put(rgb_im)
+
+# ---------------------------------
+
+# from google.cloud import storage
+
+# client = storage.Client()
+# bucket = client.get_bucket('imageupload-19537')
+# blob = bucket.blob('certificate.png')
+
+# blob.upload_from_file(img)
+# # or... (no need to use pillow if you're not transforming)
+# blob.upload_from_filename(filename=img)
+
+# -------------------------
+import base64
+from io import BytesIO
+
+
+img = img.convert('RGB')
+
+buffered = BytesIO()
+img.save(buffered, format="JPEG")
+img_str = base64.b64encode(buffered.getvalue())
+
+storage.child("certifff").put(img_str,metadata)
