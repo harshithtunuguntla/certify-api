9e472e85e4276a2cdbedfa6e2f023dc77614a288
harshithtunuguntla <harshithtunuguntla@gmail.com>
2022-01-23 03:01:58 +0530

    registered models to admin

--- certify/certifyapi/admin.py
+++ certify/certifyapi/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Referrals, UserData
+from .models import Referrals, UserData, Certificates, Event_Certificates, Events
 # Register your models here.
 
 
 admin.site.register(Referrals)
 admin.site.register(UserData)
+admin.site.register(Certificates)
+admin.site.register(Event_Certificates)
+admin.site.register(Events)
+
